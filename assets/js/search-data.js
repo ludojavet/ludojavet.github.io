5ab271a65567496140b72e392aac220225c36c4c
ludojavet <ludojavet@startmail.com>
2024-12-29 18:35:49 +0000

    Deploying to gh-pages from @ ludojavet/ludojavet.github.io@f984102866f0525f79c696fd943ce9352be02e7e 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -382,13 +382,6 @@ ninja.data = [{
           window.open("https://orcid.org/0009-0009-0233-6900", "_blank");
         },
       },{
-        id: 'social-rss',
-        title: 'RSS Feed',
-        section: 'Socials',
-        handler: () => {
-          window.open("/feed.xml", "_blank");
-        },
-      },{
       id: 'light-theme',
       title: 'Change theme to light',
       description: 'Change the theme of the site to Light',
